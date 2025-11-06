a8091c9ee967b5fc2f8adcb5ea2d23607ef58834
faruksiraje1469 <faruksiraje@gmail.com>
2025-11-06 18:12:20 +0600

    summary file

---
+++ src/summary.jsx
@@ -0,0 +1,12 @@
+// fetch (api link)
+//    .then(res => res.json())
+//     .then(data => console.log(data));
+
+
+
+//     const loadData = async () => {
+    
+//         const response = await fetch(api link);
+//         const data = response.json();
+//         return data;
+//     }
